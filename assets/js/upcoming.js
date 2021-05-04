1cf4a510943d5cd254cc8303c507fbe9874a5fa4
Hugh Bowie IV <hughbowie@me.com>
2021-05-04 16:34:27 -0400

    Update upcoming.js

--- assets/js/upcoming.js
+++ assets/js/upcoming.js
@@ -1,26 +1,12 @@
-const API_KEY = '4b52b8d4864cf35092c3095fb398a5e0';
-var apiTest = function () {
-	// format the api url
-	var apiUrl = 'https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY + '&language=en-US';
+const apiKey = '4b52b8d4864cf35092c3095fb398a5e0';
+const qURL = 'https://api.themoviedb.org/3/movie/upcoming?language=en-US&api_key=' + apiKey;
 
-	// make a get request to url
-	fetch(apiUrl)
-		.then(function (response) {
-			// request was successful
-			if (response.ok) {
-				console.log(response);
-				response.json().then(function (data) {
-					console.log(data);
-				});
-			} else {
-				// head to error page
-				console.log('error: in else');
-			}
-		})
-		.catch(function (error) {
-			// head to error page
-			console.log('error: in catch');
-		});
-};
-
-apiTest();
+const getBtn = $('#getBtn').on('click', function () {
+	$.ajax({
+		url: qURL,
+		method: 'GET',
+	}).then(function (movies) {
+		console.log(movies);
+		
+	});
+});
