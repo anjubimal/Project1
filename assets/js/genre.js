4f2fdddac97e43900c52476c18d4a419bf00f456
royster <rcantu786@gmail.com>
2021-05-05 12:09:58 -0500

    display movie images completed

--- assets/js/genre.js
+++ assets/js/genre.js
@@ -1,5 +1,6 @@
 const API_KEY = '4b52b8d4864cf35092c3095fb398a5e0';
 const BASE_URL = 'https://api.themoviedb.org/3';
+const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 
 const main = document.querySelector('#main');
 
@@ -29,6 +30,7 @@ const instancesDropdown= M.Dropdown.init(elemsDropdown, {
 });
 
 var targetHandler = function(event) {
+  event.preventDefault();
   if (event.target.id) {
     getMovieList(event.target.id);
   } 
@@ -37,23 +39,50 @@ var targetHandler = function(event) {
 function getMovieList(genreID) {
   console.log("searching for id: " + genreID);
   // use this url to gather list given genre ID when user clicks nav bar
-  var apiUrl = "https://api.themoviedb.org/3/discover/movie?api_key=" + API_KEY + "&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&with_genres=" + genreID + "&with_watch_monetization_types=flatrate";
+  var apiUrl = "https://api.themoviedb.org/3/discover/movie?api_key=" + API_KEY 
+                  + "&language=en-US&sort_by=popularity.desc&include_adult=true&with_genres=" + genreID;
   
 
   fetch(apiUrl)
     .then(function(response) {
       if (response.ok) {
         response.json().then(function(data) {
-          console.log(data);
+          const movies = data.results;
+          const movieBlock = displayMovieList(movies);
+          main.appendChild(movieBlock);
         });
       } else {
-        console.log('wtttff');
       }
     })
     .catch(function(error) {
-      console.log('alksjdl');
     });
 };
 
+function movieSection(movies) {
+  return movies.map((movie) => {
+    if (movie.poster_path) {
+      return `
+          <img class="zoom" src=${IMG_URL + movie.poster_path} data-movie-id=${movie.id}/>
+      `;
+    }
+  }) 
+}
+
+
+function displayMovieList(movies) {
+  const movieElement = document.createElement('div');
+  movieElement.setAttribute('class', 'movie');
+
+  const movieTemplate = `
+      <div class="movie-list">
+        ${movieSection(movies)}
+      </div>
+      <div class="content">
+        <p id="content-close">X</p>
+      </div>`;
+
+      movieElement.innerHTML = movieTemplate;
+      return movieElement;
+}
 
 pickedGenre.addEventListener("click", targetHandler);
